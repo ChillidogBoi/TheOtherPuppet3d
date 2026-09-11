d5476d44229b00f2e1f49aa17c015be8ef9f9044
P C <pjcpjcp@Outlook.com>
2026-09-11 11:33:52 -0500

    add font files

--- the-other-puppet-3d/Code/ui/main_menu.gd
+++ the-other-puppet-3d/Code/ui/main_menu.gd
@@ -1,7 +1,17 @@
 extends ColorRect
 
 @export var initial_button: Button
+@export var loading_screen: AnimationPlayer
+@export var transitions: AnimationPlayer
+@export_category("menus")
+
 
 func _ready():
+	assert(loading_screen != null, "Exported variable \"loading_screen\" not initialized to type \"AnimationPlayer\"")
+	loading_screen.play_backwards("fade_in")
 	assert(initial_button != null, "Exported variable \"initial_button\" not initialized to type \"Button\"")
 	initial_button.grab_focus()
+
+
+func _on_options_pressed():
+	pass
